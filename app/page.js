84c667d1d63b1399eae90c0aa98c822d763f9c44
Vivek1164 <vivekkumar1164@gmail.com>
2025-04-16 21:45:43 +0530

    hiiiiii

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import SmartWay from "./components/Home/SmartWay";
 import Footer from "./components/Home/Footer";
 
 export default function Home() {
-  console.log("hello");
+  console.log("hellooo");
   return (
     <div className="">
       <div className="hidden w-full md:flex justify-between px-24 py-2 ">
